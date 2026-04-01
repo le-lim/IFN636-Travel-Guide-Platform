d78d00d1f6e477a14d7144477396a7cbbf20364e
Le <limleer@gmail.com>
2026-04-02 06:12:31 +1000

    feature: itinerary CRUD is added at backend routes and model. CRUD tested with ThunderBolt. Added frontend node_modules to .gitignore file

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
-//app.use('/api/tasks', require('./routes/taskRoutes'));
+app.use('/api/itineraries', require('./routes/itineraryRoutes'));
+
 
 // Export the app object for testing
 if (require.main === module) {
