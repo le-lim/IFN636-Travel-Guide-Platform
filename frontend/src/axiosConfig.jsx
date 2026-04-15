17357145a9c9980ae1fd6a0ad3110f391950b233
Le <limleer@gmail.com>
2026-04-15 23:34:46 +1000

    Update title to travel guide platform; upalod new axious baseURL

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.27.189.231:5001', // live
+  baseURL: 'http://13.211.144.175:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
