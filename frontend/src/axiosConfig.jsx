141205999d46f9417ddd7d284d92021c60c5ec64
Le <limleer@gmail.com>
2026-04-16 08:57:44 +1000

    update EC2 public IP

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://13.211.144.175:5001', // live
+  baseURL: 'http://13.236.191.73:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
